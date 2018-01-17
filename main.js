8dff7a7d9b2074ffbd7dc4ecb17473ff92b9ecbf
EricGro <35313867+EricGro@users.noreply.github.com>
2018-01-17 20:55:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const navbutton = document.getElementById("navbutton");
 const navbar = document.getElementById("navbar");
 
 navbutton.addEventListener("click", () => {
-  alert.(navbar.style.display);
+  alert(navbar.style.display);
 });
 
 window.addEventListener("resize", () => {
