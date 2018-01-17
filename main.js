dd633ee8fc3a73f90505d8367cd19768aa7e394c
EricGro <35313867+EricGro@users.noreply.github.com>
2018-01-17 21:33:32 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ const navbar = document.getElementById("navbar");
 navbar.style.display = "none";
 
 navbutton.addEventListener("click", () => {
-  if(navbar.style.display === "none") navbar.style.display = "block";
-  else navbar.style.display = "none";
+  navbar.style.display === "none" ? navbar.style.display = "block" : navbar.style.display = "none";
 });
 
 window.addEventListener("resize", () => {
