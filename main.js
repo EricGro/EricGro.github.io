887b614ff3513443a3c0bdc5bb9e2226bbe37c01
EricGro <35313867+EricGro@users.noreply.github.com>
2018-03-31 22:04:30 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ const navbutton = document.getElementById("navbutton");
 const navbar = document.getElementById("navbar");
 navbar.style.display = "none";
 
-const modals = document.getElementsByClassName("mdl");
-const modalBox = document.getElementsByClassName("modal")[0];
-const modalImage = document.getElementById("mImg");
-const modalClose = document.getElementById("close");
-modalBox.style.display = "none";
-
-const testText = document.getElementById("tt");
+// const modals = document.getElementsByClassName("mdl");
+// const modalBox = document.getElementsByClassName("modal")[0];
+// const modalImage = document.getElementById("mImg");
+// const modalClose = document.getElementById("close");
+// modalBox.style.display = "none";
 
 navbutton.addEventListener("click", (event) => {
   const e = event.currentTarget;
@@ -16,19 +14,22 @@ navbutton.addEventListener("click", (event) => {
   console.log(getComputedStyle(e, null).display + " : " + e.nodeName);
 }, false);
 
-Array.prototype.forEach.call(modals, function(element) {
-  element.addEventListener("click", (event) => {
-    modalImage.src = event.currentTarget.src;
-    modalBox.style.display = "block";
-  })
-});
+// Array.prototype.forEach.call(modals, function(element) {
+//   element.addEventListener("click", (event) => {
+//     modalImage.src = event.currentTarget.src;
+//     modalBox.style.display = "block";
+//   })
+// });
 
-modalClose.addEventListener("click", (event) => {
-  const e = event.currentTarget;
-  modalBox.style.display = "none";
-  console.log(getComputedStyle(e, null).display + " : " + e.nodeName);
-}, false);
+// modalClose.addEventListener("click", (event) => {
+//   const e = event.currentTarget;
+//   modalBox.style.display = "none";
+//   console.log(getComputedStyle(e, null).display + " : " + e.nodeName);
+// }, false);
 
 window.addEventListener("resize", () => {
-  console.log(testText.innerHTML);
+  if(getComputedStyle(navbutton, null).display === "none") {
+    navbar.style.display = "none";
+    console.log("YES");
+  } else console.log("NO");
 });
